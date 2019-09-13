276e0834535fb2b66441a49a509fe7166cf59829
gafurmat000 <gafurmat000@CAM-LAB-14.bgsd.k12.wa.us>
2019-09-13 07:51:51 -0700

    Complete. later

--- src/OldLady.java
+++ src/OldLady.java
@@ -3,7 +3,13 @@ public class OldLady {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("");
+		System.out.println("  _______");
+		System.out.println(" /       \"");
+		System.out.println("/         \"");
+		System.out.println("\         /");
+		System.out.println(" \       /");
+		System.out.println(" ")
+			
 		
 
 	}
