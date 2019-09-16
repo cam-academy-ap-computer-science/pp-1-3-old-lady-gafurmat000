8c90e2523718914b7d7ae67b1803e1cd891ed0bf
GAFUR <GAFUR@192.168.0.29>
2019-09-15 23:20:33 -0700

    Finished Homework Old lady.

--- src/OldLady.java
+++ src/OldLady.java
@@ -2,16 +2,95 @@
 public class OldLady {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("  _______");
-		System.out.println(" /       \"");
-		System.out.println("/         \"");
-		System.out.println("\         /");
-		System.out.println(" \       /");
-		System.out.println(" ")
-			
+		Swallowedfly();
+		Nocluewhyfly();
+		Perhapsshedie();
 		
-
+		SwallowedSpider();
+		WriggledJiggled();
+		CatchFly();
+		Nocluewhyfly();
+		Perhapsshedie();
+		
+		SwallowedBird();
+		Absured();
+		Catchspider();
+		CatchFly();
+		Nocluewhyfly();
+		Perhapsshedie();
+		
+		Swallowedcat();
+		Imagineswallowcat();
+		Catchbird();
+		Catchspider();
+		CatchFly();
+		Nocluewhyfly();
+		Perhapsshedie();
+		
+		SwallowedDog();
+		Hogswallow();
+		CatchCat();
+		Catchbird();
+		Catchspider();
+		CatchFly();
+		Nocluewhyfly();
+		Perhapsshedie();
+		
+		SwallowedHorse();
+		Died();
+	}
+	public static void Swallowedfly(){
+		System.out.println("There was an old lady who swallowed a fly.");
+	}
+	public static void Nocluewhyfly() {
+		System.out.println("I don't know why she swallowed that fly,");
+	}
+	public static void Perhapsshedie() {
+		System.out.println("Perhaps she'll die.");
+	}
+	public static void SwallowedSpider() {
+		System.out.println("There was an old lady who swallowed a spider,");
+	}
+	public static void WriggledJiggled() {
+		System.out.println("That wriggled and iggled and jiggled inside her.");
+	}
+	public static void CatchFly(){
+		System.out.println("She swallowed the spider to catch the fly,");
+	}
+	public static void SwallowedBird() {
+		System.out.println("There was an old lady who swallowed a bird,");
+	}
+	public static void Absured() {
+		System.out.println("How absured to swallow a bird.");
+	}
+	public static void Catchspider() {
+		System.out.println("She swallowed the bird to catch the spider,");
+	}
+	public static void Swallowedcat() {
+		System.out.println("There was an old lady who swallowed a cat,");
+	}
+	public static void Imagineswallowcat() {
+		System.out.println("Imagine that to swallow a cat");
+	}
+	public static void Catchbird() {
+		System.out.println("She swallowed the cat to catch the bird,");
+	}
+	public static void SwallowedDog() {
+		System.out.println("There was an old lady who swallowed a dog,");
+	}
+	public static void Hogswallow() {
+		System.out.println("What a hog to swallow a dog.");
+	}
+	public static void CatchCat() {
+		System.out.println("She swallowed the dog to catch the cat,");
+	}
+	public static void SwallowedHorse() {
+		System.out.println("There was an old lady who swallowed a horse,");
+	}
+	public static void Died() {
+		System.out.println("She died of course.");
+	}
+		
 	}
 
-}
+
